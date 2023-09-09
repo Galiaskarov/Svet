0d0fd919ad8e62ef58f835886200547fc12e3bf8
Galiaskarov <119331000+Galiaskarov@users.noreply.github.com>
2023-09-09 14:04:12 +0700

    Создал layout

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import {Link} from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div class="pt-3 pb-3 flex justify-between items-center pl-5 pr-5 shadow">
+        <Link href="/" class="flex items-center gap-2">
+            <svg class="h-10 text-amber-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path fill-rule="evenodd" d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z" clip-rule="evenodd" />
+            </svg>
+            <span class="text-xl font-black text-amber-500">Огненный shop</span>
+        </Link>
+        <ul class="flex gap-3.5">
+            <li>
+                <Link href="/" class="flex items-center gap-0.5">
+                    <svg class="h-4 text-amber-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>
+                    <span class="text-amber-600">Товары</span>
+                </Link>
+            </li>
+            <li>
+                <Link href="/" class="flex items-center gap-0.5">
+                    <svg class="h-4 text-amber-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                    </svg>
+                    <span class="text-amber-600">Корзина</span>
+                </Link>
+            </li>
+            <li>
+                <Link href="/" class="flex items-center gap-0.5">
+                    <svg class="h-4 text-amber-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                    </svg>
+                    <span class="text-amber-600">Профиль</span>
+                </Link>
+            </li>
+        </ul>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from "@/Components/Header.vue";
+</script>
+
+<template>
+    <div class="min-h-screen bg-[url('/resources/js/Components/bg.svg')] bg-no-repeat bg-bottom bg-fixed bg-gray-50">
+        <Header/>
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
